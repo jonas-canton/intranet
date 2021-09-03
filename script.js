4386053af15ce99864ea942f6c30cb1c4e746422
Jonas Canton <jonas.cant@gmail.com>
2021-09-02 23:05:08 -0300

    Criação de cards de forma dinâmica

--- script.js
+++ script.js
@@ -3,9 +3,23 @@ const myRequest = new Request('systems.json');
 fetch(myRequest)
   .then(response => response.json())
   .then(data => {
-    for (const product of data.sistemas) {
-      console.log(product.titulo)
+    for (const system of data.systems) {
+      createCard(system);
     }
   })
   .catch(console.error);
 
+function createCard(system) {
+  const card = document.querySelector('section.card.model');
+
+  const cloneCard = card.cloneNode(true);
+  cloneCard.classList.remove('model');
+
+  cloneCard.querySelector('.card-title').innerText = system.title;
+  cloneCard.querySelector('.card-description').innerText = system.description;
+  cloneCard.querySelector('.card-button').href = system.url;
+  cloneCard.querySelector('.card-icon img').src = system.logo;
+
+  const section = document.querySelector('.cards');
+  section.appendChild(cloneCard);
+}
